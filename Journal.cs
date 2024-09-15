112725a3d003b260f95e21ec645fb3eb44cc125f
Pratyush Kumar Singh <pratyushkumar@carestack.com>
2024-09-15 16:08:44 +0530

    ⚡SRP Principal Initialization

---
+++ Journal.cs
@@ -0,0 +1,24 @@
+﻿namespace SRP
+{
+	public class Journal
+	{
+		private readonly List<string> _entries = new();
+
+		private static int count = 0;
+
+		public void AddEntry(string text)
+		{
+			_entries.Add($"{++count}: {text}");
+		}
+
+		public void RemoveEntry(int index)
+		{
+			_entries.RemoveAt(index);
+		}
+
+		public void SaveEntry(string fileName, bool overWrite = false)
+		{
+			File.WriteAllText(fileName, _entries[_entries.Count - 1]);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using SRP;
+
+var journal = new Journal();
+journal.AddEntry("I did something today!");
+journal.SaveEntry("File 1.txt");
