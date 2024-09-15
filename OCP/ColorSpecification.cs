a4654cf89ef346a839c4a59f36a98b421bb0705e
Pratyush Kumar Singh <prtsh004@gmail.com>
2024-09-15 18:02:15 +0530

    Using specification to follow Open close principal

---
+++ OCP/ColorSpecification.cs
@@ -0,0 +1,16 @@
+﻿namespace OCP
+{
+	public class ColorSpecification : ISpecification<Product>
+	{
+		private readonly Color _color;
+
+		public ColorSpecification(Color color)
+		{
+			_color = color;
+		}
+		public bool IsSatisfied(Product product)
+		{
+			return product.Color == _color;
+		}
+	}
+}
--- OCP/Program.cs
+++ OCP/Program.cs
@@ -8,7 +8,7 @@ var productList = new List<Product>
 	new Product("Banana", Color.YELLOW, Size.MEDIUM),
 	new Product("Cherry", Color.RED, Size.SMALL),
 };
-
+#region Product Filter Approach 1
 var filter = new ProductFilter();
 
 var redProduct = filter.ProductFilterByColor(productList, Color.RED);
@@ -32,3 +32,30 @@ foreach (var item in redProduct3)
 	Console.Write("Filter by Color: RED and Size SMALL ");
 	Console.WriteLine(item);
 }
+#endregion
+
+#region Product Filter Approach 2
+
+var pf = new BetterFilter();
+Console.WriteLine("*******Approach 2*******");
+foreach (var redItems in pf.Filter(productList, new ColorSpecification(Color.RED)))
+{
+	Console.Write("Approach 1 -> Filter by Color: RED ");
+	Console.WriteLine(redItems);
+}
+
+foreach (var smallSize in pf.Filter(productList, new SizeSpecification(Size.SMALL)))
+{
+	Console.Write("Approach 1 -> Filter by Size: Small ");
+	Console.WriteLine(smallSize);
+}
+
+foreach (var smallSize in pf.Filter(productList,
+	new AndSpecification<Product>(new ColorSpecification(Color.RED),
+									new SizeSpecification(Size.SMALL))))
+{
+	Console.Write("Approach 1 -> Filter by Color: RED and Size: Small ");
+	Console.WriteLine(smallSize);
+}
+
+#endregion
