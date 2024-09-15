24d53edb6120f2b593ae2825beae1bea71b94407
Pratyush Kumar Singh <prtsh004@gmail.com>
2024-09-15 18:51:22 +0530

    LSP Implementation

---
+++ LSP/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LSP;
+Console.WriteLine("Example of Liskov Substitution Principal\n");
+
+var rectangle = new Rectangle(2, 10);
+Console.WriteLine($"Area of Rectangle : 2*10 = {rectangle.Area()}");
+
+var square = new Square(5);
+Console.WriteLine($"Area of Square : 5 = {square.Area()}");
